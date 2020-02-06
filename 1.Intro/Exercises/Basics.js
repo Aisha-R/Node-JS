4bc469f3785fb1d276d5a82041f8e61531a0194a
Aisha-R <aish0695@stud.kea.dk>
2020-02-06 09:51:48 +0100

    Added the first exercises

---
+++ 1.Intro/Exercises/Basics.js
@@ -0,0 +1,18 @@
+//Exercise 1
+
+var firstName = "Anders";
+var secondName = "Latif";
+
+//Answer
+console.log("My first name is" , firstName , "and my last name is" , secondName);
+
+
+//Exercise 2
+
+var year = "2017";
+var number  = 1;
+
+var answer = parseInt(year) + number; //or 'Number(year)' works too.
+
+//Answer
+console.log(answer); 
